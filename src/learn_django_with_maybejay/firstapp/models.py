fc13f2cb55e86193e7c3e42e970a4c8f8746ce8b
Jay Prajapati <maybejaycodes@hotmail.com>
2020-01-07 17:58:09 -0500

    Uptil views config

---
+++ src/learn_django_with_maybejay/firstapp/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Products(models.Model):
+    title = models.CharField(max_length=50, default='Product Title')
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(decimal_places=2, max_digits=5, default=0.00)
+    active = models.BooleanField(default=False)
