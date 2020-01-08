65428db43fb53d22bceb90cefc1bb0fba3adb2d4
Jay Prajapati <maybejaycodes@hotmail.com>
2020-01-07 22:52:54 -0500

    Templates and object rendering done

--- src/learn_django_with_maybejay/firstapp/views.py
+++ src/learn_django_with_maybejay/firstapp/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Products
 
 # Create your views here.
+def product_details_view(request):
+    obj = Products.objects.get(id=1)
+    # context={
+    #     'title': obj.title,
+    #     'description': obj.description,
+    #     'price': obj.price,
+    #     'active': obj.active,
+    # }
+    context = {
+        'object':obj,
+    }
+    return render(request, 'product/details.html', context)
--- src/learn_django_with_maybejay/learn_django_with_maybejay/urls.py
+++ src/learn_django_with_maybejay/learn_django_with_maybejay/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from pages import views
+from firstapp.views import product_details_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', views.home_view, name='home'),
-    path('', views.home_view, name='home')
+    #path('home/', views.home_view, name='home'),
+    #path('', views.home_view, name='home'),
+    #path('products/', product_details_view)
 ]
